96a9984ad2834beb38dcd99c13a4ecad79326657
Davronbek01 <69244985+Davronbek01@users.noreply.github.com>
2020-08-05 17:47:24 +0500

    Add files via upload

--- Source.c
+++ Source.c
@@ -1,25 +1,11 @@
-# include<stdio.h>
+#include<stdio.h>
 void main() {
-	/*double a;
-	int b;
-	float c;
-	printf("Write down float number.");
-	scanf_s("%lf", &a);
-	b = (int)a;
-	c = a - b;
-	printf("%d\n%.2f", b, c);*/
+	int a, b, c;
+	scanf_s("%d%d", &a, &b);
+	c = a;
+	a = b;
+	b = c;
+	printf("%d\n%d\n%d", a, b, c);
 
 
-
-	/*int a, b;
-	printf("Write any whple number.");
-	scanf_s("%d", &a);
-	b = a % 10;
-	printf("%d", b);*/
-
-	int a = 4000, hour, min, sec;
-	hour = a / 3600;
-	min = a / 60 - hour * 60;
-	sec = a - hour * 3600 - min * 60;
-	printf("%d", sec);
 }
